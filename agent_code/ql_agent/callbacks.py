af9a01b2d344cf020c60d638c8481edd934c6c7a
Leon Remke <remke.lcode@gmail.com>
2023-08-27 13:03:05 +0100

    add easy Q-learning based agent

---
+++ agent_code/ql_agent/callbacks.py
@@ -0,0 +1,132 @@
+import os
+import pickle
+import random
+
+import numpy as np
+
+
+ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
+
+
+def setup(self):
+    """
+    Setup your code. This is called once when loading each agent.
+    Make sure that you prepare everything such that act(...) can be called.
+
+    When in training mode, the separate `setup_training` in train.py is called
+    after this method. This separation allows you to share your trained agent
+    with other students, without revealing your training code.
+
+    In this example, our model is a set of probabilities over actions
+    that are is independent of the game state.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    """
+    # if self.train or not os.path.isfile("my-saved-model.pt"):
+    #     self.logger.info("Setting up model from scratch.")
+    #     weights = np.random.rand(len(ACTIONS))
+    #     self.model = weights / weights.sum()
+    # else:
+    #     self.logger.info("Loading model from saved state.")
+    #     with open("my-saved-model.pt", "rb") as file:
+    #         self.model = pickle.load(file)
+    if self.train or not os.path.isfile("coin-collector-qtable.pt"):
+        self.logger.info("Setting up Q-table from scratch.")
+        self.q_table = None
+    else:
+        self.logger.info("Loading Q-table from saved state.")
+        with open("my-saved-qtable.pt", "rb") as file:
+            self.q_table = pickle.load(file)
+
+
+def act(self, game_state: dict) -> str:
+    """
+    Your agent should parse the input, think, and take a decision.
+    When not in training mode, the maximum execution time for this method is 0.5s.
+
+    :param self: The same object that is passed to all of your callbacks.
+    :param game_state: The dictionary that describes everything on the board.
+    :return: The action to take as a string.
+    """
+
+    # State object structure:
+    # state = {
+    #     'round': self.round,
+    #     'step': self.step,
+    #     'field': np.array(self.arena),
+    #     'self': agent.get_state(),
+    #     'others': [other.get_state() for other in self.active_agents if other is not agent],
+    #     'bombs': [bomb.get_state() for bomb in self.bombs],
+    #     'coins': [coin.get_state() for coin in self.coins if coin.collectable],
+    #     'user_input': self.user_input,
+    # }
+    if self.q_table is None:
+        state_space_size = state_to_features(game_state).shape[0]
+        self.q_table = np.zeros([state_space_size, len(ACTIONS)])  # Initialize Q-table with zeros
+
+    if self.train and np.random.rand() < self.exploration_rate:
+        self.logger.debug("Choosing action purely at random.")
+        # 80%: walk in any direction. 10% wait. 10% bomb.
+        return np.random.choice(ACTIONS, p=[.2, .2, .2, .2, .1, .1])
+    else:
+        state = state_to_features(game_state)
+        action_index = np.argmax(self.q_table[state, :])
+        return ACTIONS[action_index]
+
+
+    # # todo Exploration vs exploitation
+    # random_prob = .1
+    # if self.train and random.random() < random_prob:
+    #     self.logger.debug("Choosing action purely at random.")
+    #     # 80%: walk in any direction. 10% wait. 10% bomb.
+    #     return np.random.choice(ACTIONS, p=[.2, .2, .2, .2, .1, .1])
+
+    # self.logger.debug("Querying model for action.")
+    # return np.random.choice(ACTIONS, p=self.model)
+
+
+def state_to_features(game_state: dict) -> np.array:
+    """
+    Converts the game state to the input of your model, i.e. a feature vector.
+
+    :param game_state: A dictionary describing the current game board.
+    :return: np.array
+    """
+    if game_state is None:
+        return None
+    
+    # Extract relevant information from the game state
+    field = game_state['field']
+    player_state = game_state['self']
+    coins = game_state['coins']
+    bombs = game_state['bombs']
+    
+    # Create channels for different features
+    channels = []
+    
+    # Channel for walls and crates
+    wall_crate_channel = np.where(field == -1, 1, 0)  # Walls and crates are set to 1, rest to 0
+    channels.append(wall_crate_channel)
+    
+    # Channel for player's position
+    player_channel = np.zeros_like(field)
+    player_channel[player_state[3][1], player_state[3][0]] = 1  # Set player's position to 1
+    channels.append(player_channel)
+    
+    # Channel for coin positions
+    coin_channel = np.zeros_like(field)
+    for coin in coins:
+        coin_channel[coin] = 1  # Set coin positions to 1
+    channels.append(coin_channel)
+    
+    # Channel for bomb positions and timers
+    bomb_channel = np.zeros_like(field)
+    for bomb in bombs:
+        bomb_pos = bomb[0]
+        bomb_timer = bomb[1]
+        bomb_channel[bomb_pos] = bomb_timer  # Set bomb positions to their timers
+    channels.append(bomb_channel)
+    
+    # Stack all channels and reshape into a vector
+    stacked_channels = np.stack(channels)
+    return stacked_channels.reshape(-1)
---
+++ agent_code/ql_agent/train.py
@@ -0,0 +1,147 @@
+from collections import namedtuple, deque
+
+import pickle
+from typing import List
+
+import events as e
+from .callbacks import state_to_features
+import numpy as np
+
+# This is only an example!
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+# Hyper parameters -- DO modify
+TRANSITION_HISTORY_SIZE = 3  # keep only ... last transitions
+RECORD_ENEMY_TRANSITIONS = 1.0  # record enemy transitions with probability ...
+
+DISCOUNT_FACTOR = 0.95
+EPS = 0.5
+EPS_DECAY_FACTOR = 0.999
+LEARNING_RATE = 0.8
+NUM_EPISODES = 500
+
+ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
+
+# Events
+PLACEHOLDER_EVENT = "PLACEHOLDER"
+
+
+def setup_training(self):
+    """
+    Initialise self for training purpose.
+
+    This is called after `setup` in callbacks.py.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    """
+    # Example: Setup an array that will note transition tuples
+    # (s, a, r, s')
+    self.transitions = deque(maxlen=TRANSITION_HISTORY_SIZE)
+
+    # Set hyperparameters for agent
+    self.dicount_factor = DISCOUNT_FACTOR
+    self.eps = EPS
+    self.eps_decay_factor = EPS_DECAY_FACTOR
+    self.learning_rate = LEARNING_RATE
+    self.num_episodes = NUM_EPISODES
+
+    self.exploration_rate = 1
+    self.exploration_rate_decay = 0.99999975
+    self.exploration_rate_min = 0.1
+
+
+
+def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_state: dict, events: List[str]):
+    """
+    Called once per step to allow intermediate rewards based on game events.
+
+    When this method is called, self.events will contain a list of all game
+    events relevant to your agent that occurred during the previous step. Consult
+    settings.py to see what events are tracked. You can hand out rewards to your
+    agent based on these events and your knowledge of the (new) game state.
+
+    This is *one* of the places where you could update your agent.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    :param old_game_state: The state that was passed to the last call of `act`.
+    :param self_action: The action that you took.
+    :param new_game_state: The state the agent is in now.
+    :param events: The events that occurred when going from  `old_game_state` to `new_game_state`
+    """
+    self.logger.debug(f'Encountered game event(s) {", ".join(map(repr, events))} in step {new_game_state["step"]}')
+
+    # # Idea: Add your own events to hand out rewards
+    # if ...:
+    #     events.append(PLACEHOLDER_EVENT)
+
+    # state_to_features is defined in callbacks.py
+    self.transitions.append(Transition(state_to_features(old_game_state), self_action, state_to_features(new_game_state), reward_from_events(self, events)))
+
+
+    # From turorial:
+    # q_table[state, action] += reward + learning_rate * (discount_factor * np.max(q_table[new_state, :]) - q_table[state, action])
+    # Calculate reward from events
+    reward = reward_from_events(self, events)
+
+    # Update Q-table using Q-learning
+    old_state = state_to_features(old_game_state)
+    new_state = state_to_features(new_game_state)
+    old_q_value = self.q_table[old_state, ACTIONS.index(self_action)]
+    max_future_q = np.max(self.q_table[new_state, :])
+    new_q_value = (1 - self.learning_rate) * old_q_value + self.learning_rate * (reward + self.discount_factor * max_future_q)
+    self.q_table[old_state, ACTIONS.index(self_action)] = new_q_value
+
+
+def end_of_round(self, last_game_state: dict, last_action: str, events: List[str]):
+    """
+    Called at the end of each game or when the agent died to hand out final rewards.
+    This replaces game_events_occurred in this round.
+
+    This is similar to game_events_occurred. self.events will contain all events that
+    occurred during your agent's final step.
+
+    This is *one* of the places where you could update your agent.
+    This is also a good place to store an agent that you updated.
+
+    :param self: The same object that is passed to all of your callbacks.
+    """
+    self.logger.debug(f'Encountered event(s) {", ".join(map(repr, events))} in final step')
+    self.transitions.append(Transition(state_to_features(last_game_state), last_action, None, reward_from_events(self, events)))
+
+    # Store the model
+    # with open("my-saved-model.pt", "wb") as file:
+    #     pickle.dump(self.model, file)
+
+    # Calculate reward from events
+    reward = reward_from_events(self, events)
+
+    # Update Q-table using Q-learning for the last action-state pair
+    old_state = state_to_features(last_game_state)
+    old_q_value = self.q_table[old_state, ACTIONS.index(last_action)]
+    new_q_value = old_q_value + self.learning_rate * (reward - old_q_value)
+    self.q_table[old_state, ACTIONS.index(last_action)] = new_q_value
+
+    # Store the Q-table
+    with open("coin-collector-qtable.pt", "wb") as file:
+        pickle.dump(self.q_table, file)
+
+
+def reward_from_events(self, events: List[str]) -> int:
+    """
+    *This is not a required function, but an idea to structure your code.*
+
+    Here you can modify the rewards your agent get so as to en/discourage
+    certain behavior.
+    """
+    game_rewards = {
+        e.COIN_COLLECTED: 10,
+        e.KILLED_OPPONENT: 5,
+        e.GOT_KILLED: -5
+    }
+    reward_sum = 0
+    for event in events:
+        if event in game_rewards:
+            reward_sum += game_rewards[event]
+    self.logger.info(f"Awarded {reward_sum} for events {', '.join(events)}")
+    return reward_sum
